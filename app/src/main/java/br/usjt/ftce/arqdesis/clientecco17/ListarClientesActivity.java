6c4a19027d29eaa3c0158e87763c2d2e88d4fa84
marcosteo87@gmail.com <b3e681af>
2017-10-19 20:42:03 -0200

    Finalizado

---
+++ app/src/main/java/br/usjt/ftce/arqdesis/clientecco17/ListarClientesActivity.java
@@ -0,0 +1,29 @@
+package br.usjt.ftce.arqdesis.clientecco17;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ListarClientesActivity extends Activity {
+
+    ArrayList<String> clientes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_clientes);
+        Intent intent =getIntent();
+        String chave =(String)intent.getStringExtra(MainActivity.chave);
+        clientes = Data.buscaClientes(chave);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, clientes);
+        ListView listView = (ListView)findViewById(R.id.lista_clientes);
+        listView.setAdapter(adapter);
+
+    }
+}
--- app/src/main/java/br/usjt/ftce/arqdesis/clientecco17/MainActivity.java
+++ app/src/main/java/br/usjt/ftce/arqdesis/clientecco17/MainActivity.java
@@ -1,13 +1,26 @@
 package br.usjt.ftce.arqdesis.clientecco17;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+    EditText texte;
+    public static final String chave = "br.usjt.ftce.arqdesis.clientecco17.chave";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        texte = (EditText)findViewById(R.id.buscar_clientes);
+
+    }
+    public void buscarCliente(View view){
+        Intent intent = new Intent(this, ListarClientesActivity.class);
+        String texto = texte.getText().toString();
+        intent.putExtra(chave,texto);
+        startActivity(intent);
+
     }
 }
